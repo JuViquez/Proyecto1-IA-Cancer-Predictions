c87dee649038a2f03ff26c54518454480800f2b6
joseant12 <joseantonio122@gmail.com>
2018-09-27 11:44:12 -0600

    Arbol de decisión creado, falta retornar predicción

---
+++ Leaf.py
@@ -0,0 +1,8 @@
+class Leaf():
+    def __init__(self,
+                prediction,
+                question,
+                column):
+        self.prediction = prediction
+        self.question = question
+        self.column = column
--- Node.py
+++ Node.py
@@ -1,4 +1,5 @@
 import math
+from Leaf import Leaf
 
 class Node():
     def __init__(self,
@@ -9,6 +10,7 @@ class Node():
         self.branch = []
         self.column = 0
         self.gain = 0
+        self.question = None
         
     def count_rows(self,array_type,column):
         counter = {}
@@ -49,68 +51,78 @@ class Node():
     def split_dataset(self,value):
         array_return = []
         classification_return = []
-        for i in range(len(self.subdataset)):
-            if self.subdataset[i][self.column] == value:
-                self.subdataset[i].pop(self.column)
-                array_return.append(self.subdataset[i])
+        subdataset_copy = self.subdataset.copy()
+        for i in range(len(subdataset_copy)):
+            if subdataset_copy[i][self.column] == value:
+                del subdataset_copy[i][self.column]
+                array_return.append(subdataset_copy[i])
                 classification_return.append(self.classification[i])
         return array_return,classification_return
 
-    def tree_learning(self):
-        classification_values = self.count_rows(self.classification,0)
-        if (len(classification_values) < 2):
-            print(classification_values)
-            return 0
-        if(len(self.subdataset) == 0):
-            #return plurality of father
-            print("Pluralidad pdre")
-            return 0
-        if(len(self.subdataset[0]) == 0):
-            print("pluralidad")
-            print (classification_values)
-            return 0
+    def plurality(self,prediction):
+        default = {'Default':0}
+        for tag in prediction:
+            if (prediction[tag]>=default[list(default)[0]]):
+                default = {tag : prediction[tag]}
+        return default
 
+    def tree_learning(self,num):
+        classification_values = self.count_rows(self.classification,0)
+        difnum =+ num + 1
+        if (len(classification_values) < 2):
+            return classification_values
+        if(len(self.subdataset) == 0):
+            return 1
+        if(len(self.subdataset[0]) == 0):
+            return classification_values
         self.best_gain()
         if(self.gain == 0):
             print("Ganancia cero")
-            return 0
-
+            return 4
         column_values = self.count_rows(self.subdataset,self.column)
         for key in column_values:
             dat,clas = self.split_dataset(key)
-            tree_node = Node(dat,clas)
-            print("----------------")
-            tree_node.tree_learning()
+            tree_node = Node(dat.copy(),clas.copy())
+            tree_node.question = key
             self.branch.append(tree_node)
+            del tree_node #probar
+        for i in range(len(self.branch)):
+            response = self.branch[i].tree_learning(difnum)
+            if isinstance(response,dict):
+                self.branch[i] = Leaf(self.plurality(response),self.branch[i].question)
+        return 0
         
                
-
+columnas = ['Tipo', 'Curso', 'Interes', 'Tiempo Disponible', 'Conocimiento','Clasificador']
 
 classification = ['Si','Si','No','Si','Si','No','No','Si','No','Si','Si','No','No','Si','No','Si','Si','Si','Si','Si','No']
 
 dataset = [
-        ["Proyecto corto", 'IA', 'Medio', 'Alto', 'Medio','Si'],
-        ["Examen", 'IA', 'Alto', 'Alto', 'Bajo','Si'],
-        ["Tarea", 'Seminario','Medio', 'Bajo', 'Alto','No'],
-        ["Proyecto", 'AP', 'Bajo', 'Medio', 'Alto','Si'],
-        ["Tarea", 'Seminario', 'Alto', 'Alto', 'Medio','Si'],
-        ["Proyecto corto", 'AP', 'Medio', 'Medio', 'Bajo','No'],
-        ["Examen", 'Seminario', 'Bajo', 'Medio', 'Alto', 'No'],
-        ["Proyecto", 'AP', 'Bajo', 'Alto', 'Bajo','Si'],
-        ["Proyecto", 'Redes', 'Medio', 'Medio', 'Medio', 'No'],
-        ["Examen", 'Seminario', 'Bajo', 'Bajo', 'Medio', 'Si'],
-        ["Proyecto corto", 'IA', 'Bajo', 'Alto', 'Bajo', 'Si'],
-        ["Examen", 'Seminario', 'Alto', 'Medio', 'Alto', 'No'],
-        ["Tarea", 'AP', 'Medio', 'Bajo', 'Bajo','No'],
-        ["Proyecto", 'IA', 'Medio', 'Medio', 'Medio', 'Si'],
-        ["Tarea", 'Redes', 'Alto', 'Medio', 'Medio','No'],
-        ["Examen", 'Redes', 'Alto', 'Bajo', 'Alto', 'Si'],
-        ["Proyecto", 'Seminario', 'Alto', 'Bajo', 'Alto', 'Si'],
-        ["Proyecto", 'IA', 'Bajo', 'Medio', 'Medio','Si'],
-        ["Tarea", 'AP', 'Medio', 'Medio', 'Bajo','Si'],
-        ["Tarea", 'IA', 'Medio', 'Medio', 'Bajo','Si'],
-        ["Examen", 'Seminario', 'Bajo', 'Medio', 'Bajo','No'],
+        ["Proyecto corto", 'IA', 'C', 'Alto', 'M'],
+        ["Examen", 'IA', 'L', 'Alto', 'B'],
+        ["Tarea", 'Seminario','S', 'Bajo', 'A'],
+        ["Proyecto", 'AP', 'S', 'Medio', 'A'],
+        ["Tarea", 'Seminario', 'L', 'Alto', 'M'],
+        ["Proyecto corto", 'AP', 'C', 'Medio', 'B'],
+        ["Examen", 'Seminario', 'S', 'Medio', 'A'],
+        ["Proyecto", 'AP', 'S', 'Alto', 'B'],
+        ["Proyecto", 'Redes', 'C', 'Medio', 'M'],
+        ["Examen", 'Seminario', 'S', 'Bajo', 'M'],
+        ["Proyecto corto", 'IA', 'S', 'Alto', 'B'],
+        ["Examen", 'Seminario', 'L', 'Medio', 'A'],
+        ["Tarea", 'AP', 'C', 'Bajo', 'B'],
+        ["Proyecto", 'IA', 'C', 'Medio', 'M'],
+        ["Tarea", 'Redes', 'L', 'Medio', 'M'],
+        ["Examen", 'Redes', 'L', 'Bajo', 'A'],
+        ["Proyecto", 'Seminario', 'L', 'Bajo', 'A'],
+        ["Proyecto", 'IA', 'S', 'Medio', 'M'],
+        ["Tarea", 'AP', 'C', 'Medio', 'B'],
+        ["Tarea", 'IA', 'C', 'Medio', 'B'],
+        ["Examen", 'Seminario', 'S', 'Medio', 'B'],
 ]
 nodo = Node(dataset,classification)
-
-nodo.tree_learning()
+data1 = [1,2,3]
+data2 = data1.copy()
+data2.pop(1)
+print(data1)
+nodo.tree_learning(0)
